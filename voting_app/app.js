58e6db617fa090a012410a48a129d0dc899ffc2c
Son Ha <son.ha.anh@bluebelt.asia>
2017-08-14 10:32:01 +0700

    add voting_app

---
+++ voting_app/app.js
@@ -0,0 +1,14 @@
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div className="uunsunstackable items">
+                Hello Simon
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
